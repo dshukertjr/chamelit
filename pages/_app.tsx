c8dc03058dd16416d79baa4c88b2c31d6c08eec1
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-02-21 21:23:37 +0900

    updated linting logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import "tailwindcss/tailwind.css";
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { ReactElement } from 'react'
+import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />
 }
 
